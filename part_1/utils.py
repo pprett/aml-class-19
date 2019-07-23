c9a7b86a93cbf592a038a6080cde5fefb20a38e3
Peter Prettenhofer <peter.prettenhofer@gmail.com>
2019-07-23 22:05:31 +0200

    wip

--- part_1/utils.py
+++ part_1/utils.py
@@ -6,18 +6,18 @@ from sklearn.base import RegressorMixin
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.linear_model import LinearRegression
 
-FIGSIZE = (11, 7)
 
-np.random.seed(0)
+FIGSIZE = (11, 7)
 
 
 def true_fn(X):
     return np.cos(1.5 * np.pi * X)
 
 
-def generate_curve_data(n_samples=30):
-    X = np.sort(np.random.rand(n_samples))
-    y = true_fn(X) + np.random.randn(n_samples) * 0.1
+def generate_curve_data(n_samples=30, seed=0):
+    rng = np.random.RandomState(seed)
+    X = np.sort(rng.rand(n_samples))
+    y = true_fn(X) + rng.randn(n_samples) * 0.1
     return X, y
 
 
@@ -25,7 +25,7 @@ def plot_data(X, y, fn=None, title=None, alpha=0.4, s=20, fig=None):
     x_plot = np.linspace(0, 1, 100)
     if fig is None:
         fig = plt.figure(figsize=FIGSIZE)
-        
+
     gt = plt.plot(x_plot, fn(x_plot), alpha=alpha, label='ground truth')
 
     plt.scatter(X, y, s=s, alpha=alpha)
@@ -38,18 +38,18 @@ def plot_data(X, y, fn=None, title=None, alpha=0.4, s=20, fig=None):
     plt.legend()
     return fig
 
-    
+
 def plot_estimator(est, fig, label="{}"):
     x_plot = np.linspace(0, 1, 100)
     plt.plot(x_plot, est.predict(x_plot[:, np.newaxis]), label=label.format(est))
     plt.legend()
-    
-    
+
+
 class PolynomialRegression(BaseEstimator, RegressorMixin):
     """A simple implementation of Polynomial Regression with scikit-learn. """
     def __init__(self, degree=1):
         self.degree = degree
-        
+
     def fit(self, X, y):
         self.polynomial_transformer_ = PolynomialFeatures(degree=self.degree, 
                                                          include_bias=False)
@@ -57,7 +57,6 @@ class PolynomialRegression(BaseEstimator, RegressorMixin):
         self.linear_regression_ = LinearRegression()
         self.linear_regression_.fit(X_prime, y)
         return self
-        
+
     def predict(self, X):
         return self.linear_regression_.predict(self.polynomial_transformer_.transform(X))
-    
